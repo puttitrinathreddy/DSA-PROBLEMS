dece464758a9b329040dabc1538c50d3438760af
Trinath Reddy <reddytrinath39@gmail.com>
2023-09-20 00:58:07 +0530

    added new question

--- ARRAYS/array.js
+++ ARRAYS/array.js
@@ -235,5 +235,41 @@ const maxProfit = function (prices) {
   return profit;
 };
 
+------------------------------------136. Single Number-----------------------------------------
+
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,1]
+Output: 1
+
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+Output: 4
+
+Example 3:
+
+Input: nums = [1]
+Output: 1
+
+=================================== SOLUTION USING XOR() =======================================
+
+const singleNumber = function(nums){
+  let result = o;
+  for(let i =0; i< nums.length;i++){
+    result ^= nums[i];
+  }
+  return result;
+}
+
+
+
+
 
 */
