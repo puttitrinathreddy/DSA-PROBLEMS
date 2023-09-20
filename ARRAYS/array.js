12a5443ace258032898df66ec13db8a25778fd42
Trinath Reddy <reddytrinath39@gmail.com>
2023-09-20 23:56:39 +0530

    added few more probs

--- ARRAYS/array.js
+++ ARRAYS/array.js
@@ -268,6 +268,107 @@ const singleNumber = function(nums){
   return result;
 }
 
+--------------------------------------283. Move Zeroes :----------------------------------------
+
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+
+========================================= SOLUTION =============================================
+
+const moveZeros = function (nums) {
+  let placeNonZeros = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[placeNonZeros] = nums[i];
+      placeNonZeros++;
+    }
+  }
+  for (let i = placeNonZeros; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+  return nums;
+};
+--------------------------------- 217. Contains Duplicate --------------------------------------
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+=========================================== SOLUTION ===========================================
+
+*** FIRST WITH TIME AND SPACE COMPLEXITY OF O(N) ***
+
+  function findDuplicates(nums) {
+  let duplicates = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (duplicates[nums[i]] === undefined) {
+      duplicates[nums[i]] = nums[i];
+    } else {
+      return true;
+    }
+  }
+  return false;
+}
+
+*** SECOND WITH SPACE COMPLEXITY DECREASED TO NONE BUT TIME COMPLEXITY INCREASED ***
+
+function containsDuplicate(nums) {
+  nums.sort((a, b) => {
+    return b - a;
+  });
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0 && nums[i - 1] === nums[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
