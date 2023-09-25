2d0e2e6a24fa33a49f9c4a173e792dde40befb77
Trinath Reddy <reddytrinath39@gmail.com>
2023-09-25 13:34:56 +0530

    corrected a typo on a question

--- ARRAYS/array.js
+++ ARRAYS/array.js
@@ -394,25 +394,27 @@ function productExceptSelf(nums) {
 }
 
 
------------------------- 238. Product of Array Except Self -------------------------------------
+---------------------------- 152. Maximum Product Subarray -------------------------------------
 
-Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+Given an integer array nums, find a 
+subarray
+ that has the largest product, and return the product.
 
-The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
-
-You must write an algorithm that runs in O(n) time and without using the division operation.
+The test cases are generated so that the answer will fit in a 32-bit integer.
 
  
 
 Example 1:
 
-Input: nums = [1,2,3,4]
-Output: [24,12,8,6]
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
 
 Example 2:
 
-Input: nums = [-1,1,0,-3,3]
-Output: [0,0,9,0,0]
+Input: nums = [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
 
 ======================================== SOLUTION ==============================================
 
